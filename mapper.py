0ba92b2cb3da1313362a9df2c08a9691466309fd
Rong <rongxiaosong@gmail.com>
2014-12-11 14:49:17 +0800

    add 'fields' param to the get_* methods; a more reasonable table name convertion

--- mapper.py
+++ mapper.py
@@ -3,6 +3,8 @@
     Author: Juan MAnuel Garcia <jmg.utn@gmail.com>
 """
 
+import re
+
 class ObjectMapper(object):
 
     SQL_PARAM_SYMBOL = "?"
@@ -11,7 +13,7 @@ class ObjectMapper(object):
         self.entity = entity
 
     def _get_pairs(self):
-        return [(k,str(v)) for k,v in self.entity.__dict__.iteritems() if not k.startswith("_") and not k.startswith("__")]
+        return [(k,str(v)) for k,v in self.entity.__dict__.iteritems() if not k.startswith("_")]
 
     def _get_names(self):
         return [pair[0] for pair in self._get_pairs()]
@@ -24,8 +26,8 @@ class ObjectMapper(object):
 
     def insert(self):
         names, values = self._get_names_values()
-        names = "%s" % ",".join(names)
-        params = ",".join(["'?'" for x in values])
+        names = ",".join(names)
+        params = ",".join(["'?'"] * len(values))
         return ("INSERT INTO %s(%s) VALUES (%s)" % (self.get_table(), names, params), ) + tuple(values)
 
     def update(self, id):
@@ -37,12 +39,24 @@ class ObjectMapper(object):
     def delete(self, id):
         return ("DELETE FROM %s WHERE %s = %s" % (self.get_table(), self.get_id(), self.SQL_PARAM_SYMBOL), id)
 
-    def get_all(self):
-        names = ", ".join(self._get_names())
+    def get_all(self, fields=[]):
+        names = self._get_names()
+        # ignore the invalid fileds
+        fields = [k for k in fields if k in names]
+        if fields:
+            names = ", ".join(fields)
+        else:
+            names = ", ".join(names)
         return "SELECT %s FROM %s" % (names, self.get_table())
 
-    def get_by_id(self, id):
-        names = ", ".join(self._get_names())
+    def get_by_id(self, id, fields=[]):
+        names = self._get_names()
+        # ignore the invalid fileds
+        fields = [k for k in fields if k in names]
+        if fields:
+            names = ", ".join(fields)
+        else:
+            names = ", ".join(names)
         return ("SELECT %s FROM %s WHERE %s = %s" % (names, self.get_table(), self.get_id(), self.SQL_PARAM_SYMBOL), id)
 
     #Overridables
@@ -51,7 +65,8 @@ class ObjectMapper(object):
     #the table name and the id_name
 
     def get_table(self):
-        return self.entity.__class__.__name__.lower()
+        # a more reasonable name
+        return re.sub('((?<=[a-z])(?=[A-Z]))', '_', self.entity.__class__.__name__).lower()
 
     def get_id(self):
-        return "id_%s" % self.entity.__class__.__name__.lower()
+        return "id_%s" % re.sub('((?<=[a-z])(?=[A-Z]))', '_', self.entity.__class__.__name__).lower()
