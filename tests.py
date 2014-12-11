eaf53cbd4e43ad6718b305cec00f9fff2955f14c
Rong <rongxiaosong@gmail.com>
2014-12-11 14:49:46 +0800

    add tests for the new parm 'fields'

--- tests.py
+++ tests.py
@@ -25,15 +25,24 @@ class TestMapper(unittest.TestCase):
 
     def test_get_by_id(self):
         self.assertEquals(self.m.get_by_id(id=1), ('SELECT age, name FROM person WHERE id_person = ?', 1))
+        self.assertEquals(self.m.get_by_id(id=1, fields=['age']),
+                ('SELECT age FROM person WHERE id_person = ?', 1))
 
     def test_select_all(self):
         self.assertEquals(self.m.get_all(), "SELECT age, name FROM person")
+        self.assertEquals(self.m.get_all(fields=['name']), "SELECT name FROM person")
 
     def test_delete(self):
         self.assertEquals(self.m.delete(id=1), ('DELETE FROM person WHERE id_person = ?', 1))
 
     def test_update(self):
         self.assertEquals(self.m.update(id=1), ("UPDATE person SET age = '?', name = '?' WHERE id_person = ?", '22', 'john', 1))
+
+    def test_get_table(self):
+        self.assertEquals(self.m.get_table(), 'person')
+
+    def test_get_id(self):
+        self.assertEquals(self.m.get_id(), 'id_person')
 
 
 unittest.main()
